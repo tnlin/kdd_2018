90c86de8418f1fea2a15f85e6b20caf46fbff133
tnlin <sss950123@gmail.com>
2018-04-29 00:39:13 +0800

    add preprocessing with weather, station data

--- tony/api_submit.py
+++ tony/api_submit.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 import requests
@@ -6,10 +5,10 @@ import requests
 files={'files': open('sample_submission.csv','rb')}
 
 data = {
-    "user_id": "",   #user_id is your username which can be found on the top-right corner on our website when you logged in.
-    "team_token": "", #your team_token.
-    "description": 'your description',  #no more than 40 chars.
-    "filename": "file_name", #your filename
+    "user_id": "tnlin",   #user_id is your username which can be found on the top-right corner on our website when you logged in.
+    "team_token": "157911850c1d57e0ac45ef110ed2f3a823e1e962300f6f1e8e1abe7c254431dc", #your team_token.
+    "description": 'Baseline model',  #no more than 40 chars.
+    "filename": "submission.csv", #your filename
 }
 
 url = 'https://biendata.com/competition/kdd_2018_submit/'
