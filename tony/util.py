1d151b2cda6d9e3ad67cb06eddeec1ad5051fd88
tnlin <sss950123@gmail.com>
2018-05-02 05:55:29 +0800

    KDD 2018 first submission!!!!!!!!

--- tony/util.py
+++ tony/util.py
@@ -1,6 +1,37 @@
 import numpy as np
 import pandas as pd
 import requests
+import pytz
+
+def SMAPE(y_pred, dtrain): 
+    y_true = dtrain.get_label()
+    return 'SMAPE', np.mean(np.abs((y_pred-y_true) / (y_pred+y_true))) * 2
+
+# def SMAPE(y_pred, dtrain):
+#     y_true = dtrain.get_label()
+#     a = np.abs(np.array(y_true) - np.array(y_pred))
+#     b = np.array(y_true) + np.array(y_pred)
+#     return 'SMAPE', 2 * np.mean(np.divide(a, b, out=np.zeros_like(a), where=b!=0, casting='unsafe'))
+
+
+def MAE(y_pred, dtrain):
+    y_true = dtrain.get_label()
+    return 'MAE', mean_absolute_error(y_pred, y_true)
+
+def get_meo_stats(df):
+    df['weekofyear'] = df.utc_time.dt.weekofyear
+    df['hour'] = df.utc_time.dt.hour
+    return df.groupby(['latitude', 'longitude', 'weekofyear', 'hour']).mean()
+
+def get_submission_init(df, city=None):
+    if city=='ld':
+        stationId = df[df.need_prediction==1].Station_ID.unique()
+    else:
+        stationId = df.Station_ID.unique()
+    start_time = pd.datetime.now(pytz.utc).strftime('%Y-%m-%d 00:00:00')
+    end_time = (pd.datetime.now(pytz.utc)-pd.DateOffset(-1)).strftime('%Y-%m-%d 23:00:00')
+    utc_time = pd.date_range(start=start_time, end=end_time, freq="1H")
+    return pd.DataFrame([[i, j] for i in utc_time for j in stationId], columns=['utc_time', 'stationId'])
 
 
 def cal_near_stations(df, nears):
@@ -25,7 +56,6 @@ def df_resample(df):
         dfs.append(df_)
     return pd.concat(dfs, ignore_index=True)
 
-
 def fetch_aq(city):
     end_time = pd.datetime.now().strftime('%Y-%m-%d-%H')
     r = requests.get("https://biendata.com/competition/airquality/" + city + "/2018-03-31-0/" + end_time + "/2k0d1d8")
@@ -40,17 +70,26 @@ def fetch_aq(city):
         df[col] = df[col].astype(float)
     return df
 
-def fetch_meo(city):
+def fetch_meo(city, time=None):
     # wget https://biendata.com/competition/meteorology/ld_grid/2018-03-30-0/2018-05-01-23/2k0d1d8 -O ld_meo_new.csv
     # wget https://biendata.com/competition/meteorology/bj_grid/2018-03-30-0/2018-05-01-23/2k0d1d8 -O bj_meo_new.csv
-    df = pd.read_csv("../input/"+ city + "_meo_grid_new.csv")
+    if time:
+        r = requests.get("http://kdd.caiyunapp.com/competition/forecast/" + city + "/" + time + "/2k0d1d8")
+        df = pd.DataFrame([i.decode('utf8').split(',') for i in r.content.splitlines()])
+        df.columns = df.iloc[0]
+        df = df.reindex(df.index.drop(0))
+    else:
+        df = pd.read_csv("../input/"+ city + "_meo_grid_new.csv")
     del df['id']
     del df['weather']
     df_grid_station = pd.read_csv("../input/"+ city + "_grid_weather_station.csv")
     df = pd.merge(df, df_grid_station, how='left', left_on='station_id', right_on='stationName')
     del df['stationName']
-    df.columns = ['stationName', 'utc_time', 'temperature', 'pressure', 'humidity', 'wind_direction', 'wind_speed/kph', 'longitude', 'latitude']
+    df.columns = ['stationName', 'utc_time', 'temperature', 'pressure', 'humidity', 'wind_direction', 'wind_speed/kph',  'latitude', 'longitude']
+    for col in [ 'temperature', 'pressure', 'humidity', 'wind_direction', 'wind_speed/kph']:
+        df[col] = df[col].astype(float)
     return df
+
 
 def get_aq(city):
     if city=="bj":
@@ -65,10 +104,12 @@ def get_aq(city):
     df = pd.concat([df, df_new], ignore_index=True)
     return df
 
-def get_meo(city):
+def get_meo(city, time=None):
+    time = pd.datetime.now(pytz.utc).strftime('%Y-%m-%d-0')
     df = pd.read_csv("../input/"+ city + "_meo_grid_historical.csv")
     df_new = fetch_meo(city)
+    df_forecast = fetch_meo(city, time)
+    df = pd.concat([df, df_new, df_forecast], ignore_index=True)
     
-    df = pd.concat([df, df_new], ignore_index=True)
     df.utc_time = pd.to_datetime(df.utc_time)
     return df
